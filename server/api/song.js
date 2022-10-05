84e956f9d96d7c41d5f51c4a769151a15742ead5
justinchiu25 <justinchiu35@gmail.com>
2022-10-05 18:39:14 -0400

    Basic routes

--- server/api/song.js
+++ server/api/song.js
@@ -1,8 +1,39 @@
 const router = require("express").Router();
+const { CommandCompleteMessage } = require("pg-protocol/dist/messages");
+const {
+    models: { Song },
+} = require("../db");
 
+// api/song
 router.get("/", async (req,res,next) => {
     console.log("song page")
     res.send("song page");
+})
+
+//Grabs song info
+// api/song/:id
+router.get("/:id", async (req,res,next) => {
+    let songId = req.params.id;
+    try {
+        const songInfo = await Song.findOne({
+            where: {
+                id: songId
+            }
+        })
+        res.status(200).send(songInfo);
+    } catch (err) {
+        next(err);
+    }
+})
+
+router.put("/:id", async (req,res,next) => {
+    let songId = req.params.id;
+    try {
+        const song = await Song.findByPk(songId);
+        res.send(await song.update({artist: "Test"}))
+    } catch (err) {
+        next(err);
+    }
 })
 
 module.exports = router;
--- server/api/user.js
+++ server/api/user.js
@@ -1,8 +1,36 @@
+const User = require("../db/models/User");
+
 const router = require("express").Router();
 
 router.get("/", async (req,res,next) => {
     console.log("user page")
     res.send("user page");
+})
+
+//Grabs user info
+// api/user/:id
+router.get("/:id", async (req,res,next) => {
+    let userId = req.params.id;
+    try {
+        const userInfo = await User.findOne({
+            where: {
+                id: userId
+            }
+        })
+        res.status(200).send(userInfo);
+    } catch (err) {
+        next(err);
+    }
+})
+
+router.put("/:id", async (req,res,next) => {
+    let userId = req.params.id;
+    try {
+        const user = await User.findByPk(userId);
+        res.send(await user.update({about: "hello"}))
+    } catch (err) {
+        next(err);
+    }
 })
 
 module.exports = router;
