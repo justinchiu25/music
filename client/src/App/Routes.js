3ab29ee29a4d61d6a9d653e5bb47b323e837bd3f
justinchiu25 <justinchiu35@gmail.com>
2023-03-08 11:26:24 -0500

    Start route

---
+++ client/src/App/Routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+
+export default function Routing() {
+    return (
+      <BrowserRouter>
+        <Routes>
+        <Route path="/">
+            
+        </Route>
+        </Routes>
+      </BrowserRouter>
+    )
+  }
