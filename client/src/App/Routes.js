e65c3252ef7c64c8430f1dad093e328afc26a717
justinchiu25 <justinchiu35@gmail.com>
2023-03-20 12:23:35 -0400

    Player

--- client/src/App/Routes.js
+++ client/src/App/Routes.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route} from "react-router-dom";
+import Homepage from './Component/Homepage';
 
 export default function Routing() {
     return (
       <BrowserRouter>
         <Routes>
         <Route path="/">
-            
+            <Route index element={<Homepage />} />
         </Route>
         </Routes>
       </BrowserRouter>
